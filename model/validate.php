4aa2f42a486f555d75342ac99912b05940b31aba
Alvin Nava <anava8@mail.greenriver.edu>
2019-05-04 20:09:38 -0400

    Add functions to validate name, age, number, and email fields

--- model/validate.php
+++ model/validate.php
@@ -1,23 +1,23 @@
 <?php
 
-function validName()
+function validName($name)
 {
-
+    return !empty($name) && ctype_alpha($name);
 }
 
-function validAge()
+function validAge($age)
 {
-
+    return !empty($age) && ctype_digit($age) && $age >= 1;
 }
 
-function validPhone()
+function validPhone($number)
 {
-
+    return !empty($number) && ctype_digit($number) && strlen($number) == 10;
 }
 
-function validEmail()
+function validEmail($email)
 {
-
+    return !empty($email) && filter_var($email, FILTER_VALIDATE_EMAIL);
 }
 
 function validOutdoor()
